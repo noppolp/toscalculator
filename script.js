98e8a5f5234ec8174cb3cdedbf2e89913d2000be
noppolp <knookcs08@gmail.com>
2016-08-23 15:41:16 +0700

    init hp calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculate(){
+  //Hp = (multiplier X (lv-1)x17)+ conx85
+  var multiplier = document.getElementById("base-class").value;
+  var level = document.getElementById("level").value;
+  var con = document.getElementById("con").value
+  var hp = (multiplier * (level-1) * 17) + (con * 85);
+  document.getElementById("result").innerHTML = hp;
+}
