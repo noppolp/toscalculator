dfc5987c65755f10f0fa4cf829ad4dbc3716cf68
noppolp <knookcs08@gmail.com>
2016-08-23 18:48:31 +0700

    apply draft formular for stat calculator

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function calculate(){
   var level = document.getElementById("level").value;
 
   //stat
-  var invesStr = document.getElementById("inves-str").value;
-  var invesCon = document.getElementById("inves-con").value;
-  var invesInt = document.getElementById("inves-int").value;
-  var invesSpr = document.getElementById("inves-spr").value;
-  var invesDex = document.getElementById("inves-dex").value;
+  var invesStr = parseInt(document.getElementById("inves-str").value);
+  var invesCon = parseInt(document.getElementById("inves-con").value);
+  var invesInt = parseInt(document.getElementById("inves-int").value);
+  var invesSpr = parseInt(document.getElementById("inves-spr").value);
+  var invesDex = parseInt(document.getElementById("inves-dex").value);
 
-  var itemStr = document.getElementById("item-str").value;
-  var itemCon = document.getElementById("item-con").value;
-  var itemInt = document.getElementById("item-int").value;
-  var itemSpr = document.getElementById("item-spr").value;
-  var itemDex = document.getElementById("item-dex").value;
+  var itemStr = parseInt(document.getElementById("item-str").value);
+  var itemCon = parseInt(document.getElementById("item-con").value);
+  var itemInt = parseInt(document.getElementById("item-int").value);
+  var itemSpr = parseInt(document.getElementById("item-spr").value);
+  var itemDex = parseInt(document.getElementById("item-dex").value);
 
   var baseStr = 0;
   var baseCon = 0;
@@ -52,11 +52,12 @@ function calculate(){
         break;
   }
 
-  var actualStr = baseStr;
-  var actualCon = baseCon;
-  var actualInt = baseInt;
-  var actualSpr = baseSpr;
-  var actualDex = baseDex;
+  var actualStr = Math.ceil(((calBonusStat(baseStr, invesStr)) * 1.6) + itemStr);
+  var actualCon = Math.ceil(calBonusStat(baseCon, invesCon) + itemCon);
+  var actualInt = Math.ceil(((calBonusStat(baseInt, invesInt)) * 1.6) + itemInt);
+  var actualSpr = Math.ceil(calBonusStat(baseSpr, invesSpr) + itemSpr);
+  var actualDex = Math.ceil(calBonusStat(baseDex, invesDex) + itemDex);
+  // Display Stat Result
   document.getElementById("actual-str").innerHTML = actualStr;
   document.getElementById("actual-con").innerHTML = actualCon;
   document.getElementById("actual-int").innerHTML = actualInt;
@@ -79,6 +80,33 @@ function calculate(){
         multiplier = 1.5;
         break;
   }
-  var hp = (multiplier * (level-1) * 17) + (actualCon * 85);
+  var hp = Math.ceil((multiplier * (level-1) * 17) + (actualCon * 85));
   document.getElementById("hp").innerHTML = hp;
+}
+
+function calBonusStat(baseStat, invesStat){
+  var result = baseStat;
+  for (var i = 0; i < invesStat; i++) {
+    result++;
+    if (result >= 1 && result <= 50) {
+      if (result%5===0) {
+        result++;
+      }
+    }else if (result >= 51 && result <= 150) {
+      if (result%4===0) {
+        result++;
+      }
+    }else if (result >= 151 && result <= 300) {
+      if (result%3===0) {
+        result++;
+      }
+    }else if (result >= 300 && result <= 500) {
+      if (result%2===0) {
+        result++;
+      }
+    }else if (result >= 501) {
+      result++;
+    }
+  }
+  return result;
 }
